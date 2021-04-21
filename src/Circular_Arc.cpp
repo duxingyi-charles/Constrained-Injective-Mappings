de91b35ee0373bd3b2eb4a7853c0b7e4a8732f3c
CharlesDu <dohoney@126.com>
2021-04-21 11:55:20 -0500

    init circular arc class

---
+++ src/Circular_Arc.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Charles Du on 4/21/21.
+//
+
+#include "Circular_Arc.h"
---
+++ src/Circular_Arc.h
@@ -0,0 +1,18 @@
+//
+// Created by Charles Du on 4/21/21.
+//
+
+#ifndef TLC_CIRCULAR_ARC_H
+#define TLC_CIRCULAR_ARC_H
+
+#include <Eigen/Dense>
+
+class Circular_Arc {
+public:
+    Circular_Arc() = default;
+    ~Circular_Arc() = default;
+
+};
+
+
+#endif //TLC_CIRCULAR_ARC_H
