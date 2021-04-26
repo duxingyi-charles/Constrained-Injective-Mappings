4bf9c5ee2c5a7c3ce7c47af5b2ae3fe9bff369a3
CharlesDu <dohoney@126.com>
2021-04-26 11:53:33 -0500

    init Arrangement class

---
+++ src/Arrangement.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Charles Du on 4/26/21.
+//
+
+#include "Arrangement.h"
---
+++ src/Arrangement.h
@@ -0,0 +1,14 @@
+//
+// Created by Charles Du on 4/26/21.
+//
+
+#ifndef TLC_ARRANGEMENT_H
+#define TLC_ARRANGEMENT_H
+
+
+class Arrangement {
+
+};
+
+
+#endif //TLC_ARRANGEMENT_H
