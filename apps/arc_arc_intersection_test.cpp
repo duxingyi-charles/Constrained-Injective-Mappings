be6d665457fbcaea4ae5ea872bb48b5f2fd7d638
CharlesDu <dohoney@126.com>
2021-04-26 22:41:47 -0500

    minor

--- apps/arc_arc_intersection_test.cpp
+++ apps/arc_arc_intersection_test.cpp
@@ -7,8 +7,8 @@
 
 int main(int argc, char const *argv[])
 {
-    Circular_Arc arc1(Point(0.5, 0.4), Point(-0.5, -0.3), 2.1);
-    Circular_Arc arc2(Point(-0.6, 0.5), Point(0.3, 0.5), 2.1);
+    Circular_Arc arc1(Point(0.538, 0.32000000000000006), Point(-0.8910065241883679, -0.45399049973954675), -3.2324853071795867);
+    Circular_Arc arc2(Point(0.1140000000000001, 0.10999999999999988), Point(0.15643446504023087, -0.9876883405951378), -3.2324853071795867);
 
     std::vector<Intersection_Point> result;
     Circular_Arc::compute_intersection(arc1, arc2, result);
