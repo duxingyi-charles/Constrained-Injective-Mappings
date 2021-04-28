5e8ad8d7f62d2565bc84549e322b82c6bbf6bc26
CharlesDu <dohoney@126.com>
2021-04-28 15:37:52 -0500

    replace double by autodiff::var; get stuck at fmod(var a): fmod is not defined for autodiff::var;

--- src/geo_util.cpp
+++ src/geo_util.cpp
@@ -4,22 +4,23 @@
 
 #include "geo_util.h"
 
-Eigen::Vector2d rotate_90deg(const Eigen::Vector2d &vec) {
+VarVec2d rotate_90deg(const VarVec2d &vec) {
 //    return Eigen::Vector2d(-(vec.y()), vec.x());
     return {-(vec.y()), vec.x()};
 }
 
 
-double angle_mod_2PI(double a) {
+var angle_mod_2PI(var a) {
     if (a >= 0) {
+        // todo: how to mod autodiff::var ?
         return fmod(a, 2*M_PI);
     } else {
         return 2*M_PI + fmod(a, 2*M_PI);
     }
 }
 
-double compute_vector_angle(const Eigen::Vector2d &p) {
-    double x = p.x(), y = p.y();
+var compute_vector_angle(const VarVec2d &p) {
+    var x = p.x(), y = p.y();
     if (x == 0) {
         if (y >=0) {
             return M_PI/2;
@@ -39,9 +40,9 @@ double compute_vector_angle(const Eigen::Vector2d &p) {
     }
 }
 
-double compute_rotation_angle(double a1, double a2) {
-    double angle1 = angle_mod_2PI(a1);
-    double angle2 = angle_mod_2PI(a2);
+var compute_rotation_angle(var a1, var a2) {
+    var angle1 = angle_mod_2PI(a1);
+    var angle2 = angle_mod_2PI(a2);
     if (angle1 <= angle2) {
         return angle2 - angle1;
     } else {
@@ -49,10 +50,10 @@ double compute_rotation_angle(double a1, double a2) {
     }
 }
 
-bool is_angle_between_ccw(double a, double a1, double a2) {
-    double angle = angle_mod_2PI(a);
-    double angle1 = angle_mod_2PI(a1);
-    double angle2 = angle_mod_2PI(a2);
+bool is_angle_between_ccw(var a, var a1, var a2) {
+    var angle = angle_mod_2PI(a);
+    var angle1 = angle_mod_2PI(a1);
+    var angle2 = angle_mod_2PI(a2);
 
     if (angle1 <= angle2) {
         return (angle >= angle1) && (angle < angle2);
@@ -61,10 +62,10 @@ bool is_angle_between_ccw(double a, double a1, double a2) {
     }
 }
 
-bool is_angle_between_cw(double a, double a1, double a2) {
-    double angle = angle_mod_2PI(a);
-    double angle1 = angle_mod_2PI(a1);
-    double angle2 = angle_mod_2PI(a2);
+bool is_angle_between_cw(var a, var a1, var a2) {
+    var angle = angle_mod_2PI(a);
+    var angle1 = angle_mod_2PI(a1);
+    var angle2 = angle_mod_2PI(a2);
 
     if (angle2 <= angle1) {
         return (angle2 < angle) && (angle <= angle1);
@@ -73,7 +74,7 @@ bool is_angle_between_cw(double a, double a1, double a2) {
     }
 }
 
-bool is_angle_between(double a, double a1, double a2) {
+bool is_angle_between(var a, var a1, var a2) {
     if (a1 < a2) {
         return is_angle_between_ccw(a,a1,a2);
     } else {
@@ -81,9 +82,9 @@ bool is_angle_between(double a, double a1, double a2) {
     }
 }
 
-double compute_total_signed_area(const std::vector<Point> &vertices,
+var compute_total_signed_area(const std::vector<Point> &vertices,
                                  const std::vector<std::pair<size_t, size_t>> &edges) {
-    double area = 0;
+    var area = 0;
     for (const auto & e : edges) {
         auto i = e.first;
         auto j = e.second;
--- src/geo_util.h
+++ src/geo_util.h
@@ -8,36 +8,43 @@
 #include <Eigen/Core>
 #include <vector>
 
-typedef Eigen::Vector2d Point;
+#include <autodiff/reverse.hpp>
+#include <autodiff/reverse/eigen.hpp>
+
+typedef Eigen::Matrix<autodiff::var,2,1> Point;
+
+typedef Eigen::Matrix<autodiff::var,2,1> VarVec2d;
+
+typedef autodiff::var var;
 
 // return the vector after rotating 90 degree counter clockwise
-Eigen::Vector2d rotate_90deg(const Eigen::Vector2d &vec);
+VarVec2d rotate_90deg(const VarVec2d &vec);
 
 // compute the angle mod 2pi
 // return: angle in [0, 2pi)
-double angle_mod_2PI(double a);
+var angle_mod_2PI(var a);
 
 // compute angle from positive x-axis to vec
 // return: angle in [0, 2pi)
-double compute_vector_angle(const Point &p);
+var compute_vector_angle(const Point &p);
 
 // compute the counter clockwise rotation angle from a1 to a2
 // return: angle in [0, 2pi)
-double compute_rotation_angle(double a1, double a2);
+var compute_rotation_angle(var a1, var a2);
 
 // for a ray sweeping counter-clockwise from angle a1(included) to a2(not included),
 // test if the ray will encounter angle a
-bool is_angle_between_ccw(double a, double a1, double a2);
+bool is_angle_between_ccw(var a, var a1, var a2);
 
 // for a ray sweeping clockwise from angle a1(included) to a2(not included),
 // test if the ray will encounter angle a
-bool is_angle_between_cw(double a, double a1, double a2);
+bool is_angle_between_cw(var a, var a1, var a2);
 
 // test if angle a is between a1(included) and a2(not included)
-bool is_angle_between(double a, double a1, double a2);
+bool is_angle_between(var a, var a1, var a2);
 
 // compute total signed area of a polygon
-double compute_total_signed_area(const std::vector<Point> &vertices,
+var compute_total_signed_area(const std::vector<Point> &vertices,
                                  const std::vector<std::pair<size_t,size_t>> &edges);
 
 
